16c2eed8cb108eccda1967052088f8a2a2fc25ce
jhhb <jboyle@bowdoin.edu>
2016-04-05 18:50:09 -0400

    still need to add segue from login / auth to map

--- Shuttle_Application/UserAuthViewController.swift
+++ Shuttle_Application/UserAuthViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class UserAuthViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +20,16 @@ class UserAuthViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
+    @IBAction func userLoggedIn(sender: AnyObject) {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+    
+        if let _ = appDelegate {
+            //segue to map
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        }
+     
     }
-    */
-
-}
+ }
